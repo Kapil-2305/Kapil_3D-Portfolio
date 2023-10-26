a221f89fbf50051e250c54b7e84e3da873a4b9d0
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-10-27 01:57:01 +0530

    corrected code in all the files

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
 function App() {
-  return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
-  );
+	return (
+		<Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+			<color attach="background" args={["#ececec"]} />
+			<Experience />
+		</Canvas>
+	);
 }
 
 export default App;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,11 +3,11 @@ import { OrbitControls } from "@react-three/drei";
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+        <OrbitControls />
+        <mesh>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
     </>
   );
 };
