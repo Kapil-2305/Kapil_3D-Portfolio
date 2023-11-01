a5bb03ed6f541c093cd0149311cbff197a56125d
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-11-01 23:45:27 +0530

    added avatar and added animations on avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,23 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
-  return (
-    <>
-        <OrbitControls />
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-    </>
-  );
+    const {animation} = useControls({
+        animation: {
+            value: "Typing",
+            options: ["Typing", "Standing", "Falling"]
+        }
+    });
+
+    return (
+        <>
+            <OrbitControls />
+            <Sky />
+            <Environment />
+            <group position-y={-1}>
+                <Avatar animation={animation}/>
+            </group>
+        </>
+    );
 };
