7ff3c71c33638d8638e9d6b2ba8feec3080eefc7
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-11-04 23:58:20 +0530

    adding Scroll control in website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Scroll, ScrollControls } from "@react-three/drei";
 
 function App() {
 	return (
-		<Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-			<color attach="background" args={["#ececec"]} />
-			<Experience />
-		</Canvas>
+		<div>
+			<Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+				<color attach="background" args={["#ececec"]} />
+
+				<ScrollControls pages={4} damping={0.1}>
+					<Experience />
+					<Scroll html>
+						<h1 className="text-3xl font-bold underline">Hello World</h1>
+					</Scroll>
+				</ScrollControls>
+			</Canvas>
+		</div>
 	);
 }
 
