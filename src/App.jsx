2e070052e89cc38c747ed58ee7ddd0063292446e
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-11-06 23:36:39 +0530

    updated Interface.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,45 @@ import Interface from "./components/Interface";
 import Menu from "./components/Menu";
 import Cursor from "./components/Cursor";
 import { Leva } from "leva";
+import {framerMotionConfig} from './config'
 
 function App() {
-	const [section, setSection] = useState(0);
-	const [menuOpened, setMenuOpened] = useState(false);
+	// const [section, setSection] = useState(0);
+	// const [menuOpened, setMenuOpened] = useState(false);
 
-	useEffect(() => {
-		setMenuOpened(false);
-	}, [section]);
+	// useEffect(() => {
+	// 	setMenuOpened(false);
+	// }, [section]);
 
 	return (
 		<>
-			<MotionConfig
+			<Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+				<color attach="background" args={["#e6e7ff"]} />
+
+				<ScrollControls pages={4} damping={0.1}>
+					{/* <ScrollManager/> */}
+					<Scroll>
+						<Experience />
+					</Scroll>
+					<Scroll html>
+						<Interface />
+					</Scroll>
+				</ScrollControls>
+			</Canvas>
+
+			
+			{/* <MotionConfig
 				transition={{
-					...framerMotionTransition,
+					...framerMotionConfig,
 				}}
 			>
-				<Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
-					<color attach="background" args={["#e6e7ff"]} />
-
-					<ScrollControls pages={4} damping={0.1}>
-						<ScrollManager/>
-						<Scroll>
-							<Experience />
-						</Scroll>
-						<Scroll html>
-							<Interface />
-						</Scroll>
-					</ScrollControls>
-				</Canvas>
+				
 
 				<Menu />
 				<Cursor />
-			</MotionConfig>
+			</MotionConfig> */}
 			
-			<Leva />
+			{/* <Leva /> */}
 		</>
 	);
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,48 +1,32 @@
-import {
-    ContactShadows,
-    Environment,
-    OrbitControls,
-    Sky,
-  } from "@react-three/drei";
-  import { useControls } from "leva";
-  import { Avatar } from "./Avatar";
+import { useControls } from "leva";
+import { Avatar } from "./Avatar";
   
-  export const Experience = () => {
-    const { animation } = useControls({
-      animation: {
-        value: "Typing",
-        options: ["Typing", "Falling", "Standing"],
-      },
-    });
+export const Experience = () => {
+        const { animation } = useControls({
+            animation: {
+                value: "Typing",
+                options: ["Typing", "Falling", "Standing"],
+            },
+        });
+
     return (
-      <>
-        <OrbitControls />
-        <Sky />
-        {/* <Environment preset="sunset" /> */}
-        <ambientLight/>
-        <group position-y={-1}>
-          <ContactShadows
-            opacity={0.42}
-            scale={10}
-            blur={1}
-            far={10}
-            resolution={256}
-            color="#000000"
-          />
-          <Avatar animation={animation} />
-          {animation === "Typing" && (
-            <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
-              <boxGeometry />
-              <meshStandardMaterial color="white" />
-            </mesh>
-          )}
-  
-          <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
-            <planeGeometry />
-            <meshStandardMaterial color="white" />
-          </mesh>
-        </group>
-      </>
+        <>
+            <ambientLight intensity={1}/>
+            <group position-y={-1}>
+                <Avatar animation={animation} />
+                {animation === "Typing" && (
+                    <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+                        <boxGeometry />
+                        <meshStandardMaterial color="white" />
+                    </mesh>
+                )}
+
+                <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+                    <planeGeometry />
+                    <meshStandardMaterial color="white" />
+                </mesh>
+            </group>
+        </>
     );
-  };
+};
   
