d140154079fa51ae0ba9fdf32c798bd37a69b4f7
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-17 02:18:20 +0530

    Add ProjectsSection component

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -222,6 +222,38 @@ const SkillsSection = () => {
     );
 };
 
+const ProjectsSection = () => {
+    const [currentProject, setCurrentProject] = useAtom(currentProjectAtom);
+  
+    const nextProject = () => {
+        setCurrentProject((currentProject + 1) % projects.length);
+    };
+  
+    const previousProject = () => {
+        setCurrentProject((currentProject - 1 + projects.length) % projects.length);
+    };
+  
+    return (
+        <Section>
+            <div className="flex w-full h-full gap-8 items-center justify-center">
+                <button
+                    className="hover:text-indigo-600 transition-colors"
+                    onClick={previousProject}
+                >
+                    ← Previous
+                </button>
+                <h2 className="text-3xl md:text-5xl font-bold">Projects</h2>
+                <button
+                    className="hover:text-indigo-600 transition-colors"
+                    onClick={nextProject}
+                >
+                    Next →
+                </button>
+            </div>
+        </Section>
+    );
+};
+
 const ContactSection = () => {
     return (
         <Section>
