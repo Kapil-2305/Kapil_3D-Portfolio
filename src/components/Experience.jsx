406e9702c66cd92fa65e3811415a7c46886c7d9a
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-17 11:33:39 +0530

    Add jotai package and update dependencies

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,19 +2,24 @@ import {
     Float,
     MeshDistortMaterial,
     MeshWobbleMaterial,
+    useScroll,
 } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { animate, useMotionValue } from "framer-motion";
 import { motion } from "framer-motion-3d";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { framerMotionConfig } from "../config";
 import { Avatar } from "./Avatar";
 import { Office } from "./Office";
 import { Background } from "./Background";
+import { Projects } from "./Projects";
   
 export const Experience = (props) => {
-    const { section, menuOpened } = props;
+    const {  menuOpened } = props;
     const { viewport } = useThree();
+    const data = useScroll();
+
+    const [section, setSection] = useState(0);
 
     const cameraPositionX = useMotionValue();
     const cameraLookAtX = useMotionValue();
@@ -28,7 +33,27 @@ export const Experience = (props) => {
         });
     }, [menuOpened]);
 
+    const characterContainerAboutRef = useRef();
+
+    const [characterAnimation, setCharacterAnimation] = useState("Typing");
+    useEffect(() => {
+        setCharacterAnimation("Falling");
+        setTimeout(() => {
+            setCharacterAnimation(section === 0 ? "Typing" : "Standing");
+        }, 600);
+    }, [section]);
+
     useFrame((state) => {
+        let curSection = Math.floor(data.scroll.current * data.pages);
+
+        if (curSection > 3) {
+            curSection = 3;
+        }
+
+        if (curSection !== section) {
+            setSection(curSection);
+        }
+
         state.camera.position.x = cameraPositionX.get();
         state.camera.lookAt(cameraLookAtX.get(), 0, 0);
     });
@@ -36,6 +61,48 @@ export const Experience = (props) => {
     return (
         <>
             <Background />
+            <motion.group
+                position={[1.9072935059634513, 0.14400000000000002, 2.681801948466054]}
+                rotation={[-3.141592653589793, 1.2053981633974482, 3.141592653589793]}
+                animate={"" + section}
+                transition={{
+                    duration: 0.6,
+                }}
+                variants={{
+                    0: {
+                        scaleX: 0.9,
+                        scaleY: 0.9,
+                        scaleZ: 0.9,
+                    },
+                    1: {
+                        y: -viewport.height + 0.5,
+                        x: 0,
+                        z: 7,
+                        rotateX: 0,
+                        rotateY: 0,
+                        rotateZ: 0,
+                    },
+                    2: {
+                        x: -2,
+                        y: -viewport.height * 2 + 0.5,
+                        z: 0,
+                        rotateX: 0,
+                        rotateY: Math.PI / 2,
+                        rotateZ: 0,
+                    },
+                    3: {
+                        y: -viewport.height * 3 + 1,
+                        x: 0.3,
+                        z: 8.5,
+                        rotateX: 0,
+                        rotateY: -Math.PI / 4,
+                        rotateZ: 0,
+                    },
+                }}
+            >
+                <Avatar animation={characterAnimation} />
+            </motion.group>
+            <ambientLight intensity={1} />
             <motion.group
                 position={[1.5, 2, 3]}
                 scale={[0.9, 0.9, 0.9]}
@@ -45,6 +112,13 @@ export const Experience = (props) => {
                 }}
             >
                 <Office section={section} />
+
+                <group
+                    ref={characterContainerAboutRef}
+                    name="CharacterSpot"
+                    position={[0.07, 0.16, -0.57]}
+                    rotation={[-Math.PI, 0.42, -Math.PI]}
+                ></group>
             </motion.group>
 
             {/* SKILLS */}
@@ -95,11 +169,9 @@ export const Experience = (props) => {
                         />
                     </mesh>
                 </Float>
-
-                <group scale={[2, 2, 2]} position-y={-1.5}>
-                    <Avatar animation={section === 0 ? "Falling" : "Standing"} />
-                </group>
             </motion.group>
+            
+            <Projects />
         </>
     );
 };
