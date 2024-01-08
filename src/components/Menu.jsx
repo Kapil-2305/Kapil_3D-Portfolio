aaed8d9a64f7303873ca0d219b63f18d07d78348
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-08 18:34:06 +0530

    Refactor Menu component styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Menu = (props) => {
     const {onSectionChange, menuOpened, setMenuOpened} = props;
 
@@ -7,7 +5,7 @@ export const Menu = (props) => {
         <>
             <button
                 onClick={() => setMenuOpened(!menuOpened)}
-                className="z-20 fixed top-12 right-12 p-3 bg-indigo-600 w-11 h-11 rounded-md"
+                className="z-20 fixed top-4 right-4 md:top-12 md:right-12 p-3 bg-indigo-600 w-11 h-11 rounded-md"
             >
                 <div
                     className={`bg-white h-0.5 rounded-md w-full transition-all ${menuOpened ? "rotate-45  translate-y-0.5" : "" }`}
@@ -21,7 +19,7 @@ export const Menu = (props) => {
             </button>
 
             <div
-                className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col ${menuOpened ? "w-80" : "w-0"}`}
+                className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col ${menuOpened ? "w-full md:w-80" : "w-0"}`}
             >
                 <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
                     <MenuButton label="About" onClick={() => onSectionChange(0)} />
