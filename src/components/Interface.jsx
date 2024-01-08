024cce9d91f62b7387af142f9f8c26050cc9f235
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-08 10:40:35 +0530

    Add @formspree/react dependency and update contact form

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { motion } from "framer-motion";
 import { useAtom } from "jotai";
 import { currentProjectAtom, projects } from './Projects';
+import { useForm, ValidationError } from '@formspree/react';
 
 const Section = (props)=>{
-    const {children} = props;
+    const {children, mobileTop} = props;
 
     return (
         <motion.section
-            className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}
+            className={`h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start ${mobileTop ? "justify-start md:justify-center" : "justify-center"}`}
             initial={{
                 opacity: 0,
                 y: 50,
@@ -42,7 +43,7 @@ export const Interface = (props) => {
 const AboutSection = (props)=>{
     const {setSection} = props;
     return (
-        <Section>
+        <Section mobileTop>
             <h1 className='text-6xl font-extrabold leading-snug'>
                 Hi, I,m
                 <br />
@@ -258,47 +259,67 @@ const ProjectsSection = () => {
 };
 
 const ContactSection = () => {
+    const [state, handleSubmit] = useForm("xeqyqwvz");
     return (
         <Section>
-            <h2 className="text-5xl font-bold">Contact me</h2>
-            <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-                <form>
-                    <label for="name" className="font-medium text-gray-900 block mb-1">
-                        Name
-                    </label>
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-                    />
-                    <label
-                        for="email"
-                        className="font-medium text-gray-900 block mb-1 mt-8"
-                    >
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-                    />
-                    <label
-                        for="email"
-                        className="font-medium text-gray-900 block mb-1 mt-8"
-                    >
-                        Message
-                    </label>
-                    <textarea
-                        name="message"
-                        id="message"
-                        className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-                    />
-                    <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-                        Submit
-                    </button>
-                </form>
+            <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
+            <div className="mt-8 p-8 rounded-md bg-white bg-opacity-50 w-96 max-w-full">
+                {state.succeeded ? (
+                    <p className="text-gray-900 text-center">
+                        Thanks for your message! I will get back to you soon.
+                    </p>
+                    ) : (
+                        <form onSubmit={handleSubmit}>
+                            <label for="name" className="font-medium text-gray-900 block mb-1">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                            />
+                            <label
+                                for="email"
+                                className="font-medium text-gray-900 block mb-1 mt-8"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                            />
+                            <ValidationError 
+                                className="mt-1 text-red-500"
+                                prefix="Email" 
+                                field="email"
+                                errors={state.errors}
+                            />
+                            <label
+                                for="email"
+                                className="font-medium text-gray-900 block mb-1 mt-8"
+                            >
+                                Message
+                            </label>
+                            <textarea
+                                name="message"
+                                id="message"
+                                className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+                            />
+                            <ValidationError 
+                                className="mt-1 text-red-500"
+                                prefix="Message" 
+                                field="message"
+                                errors={state.errors}
+                            />
+                            <button disabled={state.submitting} className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+                                Submit
+                            </button>
+                        </form>   
+                    )
+                }
             </div>
         </Section>
     );
